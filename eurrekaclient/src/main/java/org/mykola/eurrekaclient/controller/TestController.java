8fbc58437e7e7d0a6779500fd807761d5b6cbcc2
DeliriumTraemens <oskinnick@gmail.com>
2022-06-20 15:57:08 +0300

    Load Ballancer Starting Ports are Changed

--- eurrekaclient/src/main/java/org/mykola/eurrekaclient/controller/TestController.java
+++ eurrekaclient/src/main/java/org/mykola/eurrekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.mykola.eurrekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+//    ( " ${ expression here } " )
+    @Value( " ${eureka.instance.instance-id} " )
+    private String id;
+
+
     @GetMapping("/test")
     public String test(){
         return "testblablabla";
     }
 
     @GetMapping("/test2")
-        public String test2(){
+    public String test2(){
         return "Test 2aaa";
-
+    }
+    
+    @GetMapping("/port")
+    public String port(){
+        return "Instance Number is " + id;
     }
 }
